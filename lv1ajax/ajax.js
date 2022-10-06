39080f2a3380dc9819f157c62ae1ce2e3df3e518
phuongln <namphuong@gnh.edu.vn>
2022-10-06 10:32:52 +0700

    Show dữ liệu mảng lồng ob qua Json
    Khi khai báo bảng thì đối với dữ liệu mảng chỉ cần khai báo title. Đối với dữ liệu object lồng mảng thì cần khai báo title và data (key trong object)

--- lv1ajax/ajax.js
+++ lv1ajax/ajax.js
@@ -1,17 +1,34 @@
-import myJson from './data.json' assert {type: 'json'};
-// console.log(myJson[1]);
+// import myJson from './data.json' assert {type: 'json'};
+// // console.log(myJson[1]);
 
+
+// $(document).ready(function () {
+//     $('#draft').DataTable({
+//         "data": myJson,
+//         "columns": [
+//             {"title": "Name"},
+//             {"title": "Position"},
+//             {"title": "Office"},
+//             {"title": "Extn."},
+//             {"title": "Start date"},
+//             {"title": "Salary"}
+//         ]
+//     });
+// });
+
+import thoiquenJson from './datax3.json' assert {type: 'json'};
 
 $(document).ready(function () {
     $('#draft').DataTable({
-        "data": myJson,
+        "data": thoiquenJson,
         "columns": [
-            {"title": "Name"},
-            {"title": "Position"},
-            {"title": "Office"},
-            {"title": "Extn."},
-            {"title": "Start date"},
-            {"title": "Salary"}
+            { "data": "date", "title":"Ngày"},
+          { "data": "id", "title":"ID"  },
+          { "data": "ho_ten", "title":"Họ và tên"},
+          { "data": "image_lkh", "title":"LKH"},
+          { "data": "image_kaizen", "title":"Kaizen"},
+          { "data": "image_thien", "title":"Thiền"},
+          { "data": "image_theduc", "title":"Thể dục"},
         ]
-    })
-})
+    });
+});
